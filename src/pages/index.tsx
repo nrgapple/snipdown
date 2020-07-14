d8d1dbb821199754aab02c412ce0dac938ff7840
nrgapple <champtoday@gmail.com>
2020-07-14 00:44:02 -0400

    add other headers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,8 +27,10 @@ const Home = ({ code }: DataProps) => {
           const resp = await fetch(
             `https://github.com/login/oauth/access_token?client_secret=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET}&client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_KEY}&code=${code}`,
             {
+              method: "POST",
               mode: "no-cors",
               headers: {
+                "Content-Type": "application/json",
                 Accept: "application/json",
               },
             }
