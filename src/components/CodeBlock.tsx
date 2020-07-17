353e5085689e0ade1eb428c72e2fb784495f8ab1
nrgapple <champtoday@gmail.com>
2020-07-17 11:25:50 -0400

    change code style

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,4 +1,5 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { tomorrow as style } from "react-syntax-highlighter/dist/esm/styles/prism"
 
 interface Props {
   language: string
@@ -10,6 +11,7 @@ const CodeBlock = ({ language, value }: Props) => {
     <SyntaxHighlighter
       codeTagProps={{ className: "code-block" }}
       language={language}
+      style={style}
     >
       {value}
     </SyntaxHighlighter>
